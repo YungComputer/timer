b008ef2f6296596a3f7d84bfbc52f640a1b44152
CLARE NOLAN <clarenolan92@gmail.com>
2020-01-14 22:26:18 +0000

    added pair programming note

--- timer1.js
+++ timer1.js
@@ -1,10 +1,13 @@
+//pair programming with Eina
+
 const args = process.argv.slice(2);
 const timer = (times) => {
   for (const timer of times) {  
+    if (timer > 0 && typeof timer !== "number"){
     setTimeout(() => {
-      // console.log(timer, seconds);
       process.stdout.write('\x07');
     }, timer * 1000)  
   };
-}
+};
+};
 timer(args);
