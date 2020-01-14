a6c9a4951543986b50dbfda27bb48083111ff129
CLARE NOLAN <clarenolan92@gmail.com>
2020-01-14 22:15:46 +0000

    git commit -m "added beep"

--- timer1.js
+++ timer1.js
@@ -1,9 +1,10 @@
-let args = process.argv.slice(2);
-
-for (const timer of args) {
-  let seconds = timer * 1000;
-  setTimeout(() => {
-    //console.log(timer, seconds);
-    process.stdout.write('\x07');
-  }, seconds)
-};
+const args = process.argv.slice(2);
+const timer = (times) => {
+  for (const timer of times) {  
+    setTimeout(() => {
+      // console.log(timer, seconds);
+      process.stdout.write('\x07');
+    }, timer * 1000)  
+  };
+}
+timer(args);
